95e8b827cce12175cf92536b8b4f58695dad2ee0
Han Tiet <94570703+han-tiet@users.noreply.github.com>
2025-06-13 18:42:34 +0100

    feat: changed component library to materialui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,12 @@
 // tailwind.config.js
-import {heroui} from "@heroui/react";
-
-/** @type {import('tailwindcss').Config} */
-const config = {
-  content: [
-    // ...
-    // make sure it's pointing to the ROOT node_module
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
-  ],
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  darkMode: "class",
-  plugins: [heroui()]
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }
-
-export default config;
