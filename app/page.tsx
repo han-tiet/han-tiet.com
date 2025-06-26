1ccf900b36efde9440ecd61d65128d6bd3471a71
HnTt <hanweitiet@gmail.com>
2025-06-26 21:33:53 +0100

    feat: added navigation bar to index

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
-import Image from "next/image";
-import Link from "next/link";
+import * as React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import NavigationBar from './components/NavigationBar';
+
 
 export default function Index() {
   return(
-    <div className="text-center text-2xl">
-      <Link href="/gif-hunter">GIF Hunter</Link>
-    </div>
+      <>
+        <NavigationBar />
+        <div className="mt-16">
+          <div className="container text-center text-2xl">
+            <Link href="/gif-hunter">GIF Hunter</Link>
+          </div>
+        </div>
+      </>
     
   )
 }
