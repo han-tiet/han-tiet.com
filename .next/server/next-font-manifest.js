8cab4d6501595c88d580b49a917a4b97cccb3815
HnTt <hanweitiet@gmail.com>
2025-07-20 21:19:31 +0100

    refactor: reorganized folder structure to be similiar to bulletproof-react

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\n  \"app\": {\n    \"[project]/app/gif-hunter/page\": [],\n    \"[project]/app/gif-hunter/search/page\": [],\n    \"[project]/app/page\": []\n  },\n  \"appUsingSizeAdjust\": true,\n  \"pages\": {},\n  \"pagesUsingSizeAdjust\": false\n}"
--- app/gif-hunter/page.tsx
+++ app/gif-hunter/page.tsx
@@ -2,22 +2,7 @@ import React from "react"
 import Link from "@mui/material/Link"
 import Search from "../components/Search"
 
-export default async function GifHunter({
-  // searchParams : A promise that resolves to an object containing the search parameters of the current URL. 
-  searchParams, 
-  }: {
-  searchParams: Promise<{ p: string }>
-,}) {
-  const { p } = await searchParams
-
-	const giphyResp = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY_1}&q=${p}&limit=10&offset=0&rating=r&lang=en&bundle=messaging_non_clips`)
-  const tenorResp = await fetch (`https://tenor.googleapis.com/v2/search?key=${process.env.API_KEY_2}&q=${p}&client_key=gifhunter&country=GB&locale=en_GB&limit=10`)
-
-  const respJson = await Promise.all([
-    giphyResp.json(),
-    tenorResp.json(),
-  ])
-
+export default async function GifHunter() {
   return (
     <div className="mx-auto">
       <div className="text-center text-6xl font-bold my-5 ">
