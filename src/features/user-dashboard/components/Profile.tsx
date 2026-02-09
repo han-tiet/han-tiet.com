07149bc70406ff678a679f1f4c8cced29a0b9c83
Han Tiet <94570703+han-tiet@users.noreply.github.com>
2026-02-09 20:36:59 +0000

    Revert "HAN-002: Restructuring of Project Folders"

--- src/features/user-dashboard/components/Profile.tsx
+++
@@ -1,9 +0,0 @@
-import ProfileDetails from "@/features/user-dashboard/components/ProfileDetails";
-
-type Props = {
-  children: React.ReactNode;
-};
-
-export default function Profile({ children }: Props) {
-  return <ProfileDetails>{children}</ProfileDetails>;
-}
--- src/features/user-dashboard/components/ProfileDetails.tsx
+++
@@ -1,9 +0,0 @@
-import UserDetails from "@/features/user-dashboard/components/UserDetails";
-
-type Props = {
-  children: React.ReactNode;
-};
-
-export default function ProfileDetails({ children }: Props) {
-  return <UserDetails>{children}</UserDetails>;
-}
