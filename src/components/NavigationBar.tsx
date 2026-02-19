12fe8730d577ffe4f5d37a0ccce14b8615c084fc
Han Tiet <hanweitiet@gmail.com>
2026-02-19 13:38:16 +0000

    feat: added nav buttons and revamped style

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,16 +1,30 @@
-import { Box, AppBar, Toolbar, Typography } from "@mui/material";
+"use client";
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+import NavButton from "./NavButton";
+import Link from "next/link";
 
 export default function NavigationBar() {
   return (
-    <Box>
-      <AppBar position="fixed">
-        <Toolbar>
-          <Typography variant="h6" component="div">
-            han-tiet.com
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Toolbar />
-    </Box>
+    <div className="col-span-full grid grid-cols-2 items-center h-[140px] px-[50px]">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href="/" className="text-[48px]/[60px] font-semibold">
+              Han Tiet
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <div className="flex flex-row justify-self-end gap-4">
+        <NavButton href="/about">About</NavButton>
+        <NavButton href="/projects">Projects</NavButton>
+        <NavButton href="/contact">Contact</NavButton>
+      </div>
+    </div>
   );
 }
