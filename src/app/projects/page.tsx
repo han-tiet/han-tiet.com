27611e21d11729a6ab953b9d18ea9ce055985086
Han Tiet <hanweitiet@gmail.com>
2026-03-23 16:58:36 +0000

    feat: initial commit

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,11 @@
+import NavigationBar from "@/components/NavigationBar";
+import CoverFlow from "@/components/CoverFlow";
+
+export default function Projects() {
+  return (
+    <>
+      <NavigationBar />
+      <CoverFlow />
+    </>
+  );
+}
