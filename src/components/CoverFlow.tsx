3bb79137a40cbc1656dabf1ea8387afea2b9f741
Han Tiet <hanweitiet@gmail.com>
2026-03-27 11:36:35 +0000

    feat: added project titles and descriptions

--- src/components/CoverFlow.tsx
+++ src/components/CoverFlow.tsx
@@ -3,16 +3,19 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "@/app/projects/page.module.css";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 
 export default function CoverFlow() {
-  const ref = useRef<HTMLElement>(null);
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const el = ref.current;
     if (!el) return;
 
     const onWheel = (e: WheelEvent) => {
+      if (Math.abs(e.deltaY) < Math.abs(e.deltaX)) return;
+
       if (el.scrollWidth > el.clientWidth) {
         e.preventDefault();
 
@@ -23,7 +26,9 @@ export default function CoverFlow() {
 
     el.addEventListener("wheel", onWheel, { passive: false });
 
-    return () => el.removeEventListener("wheel", onWheel);
+    return () => {
+      el.removeEventListener("wheel", onWheel);
+    };
   }, []);
 
   return (
@@ -31,22 +36,59 @@ export default function CoverFlow() {
       <ul className={styles.cards}>
         <Link href="/gif-hunter">
           <li>
-            <Image
-              src="/images/gifhunter-screen.png"
-              width={2000}
-              height={1000}
-              alt="gifhunter"
-            />
+            <div>
+              <div id="title" className={styles.title}>
+                <div className="text-xl">GIFHunter - A GIF Search Engine</div>
+                <Image
+                  src="/github.svg"
+                  width={24}
+                  height={24}
+                  alt="github-link"
+                />
+              </div>
+              <Image
+                src="/images/gifhunter-screen.png"
+                width={2000}
+                height={1000}
+                alt="gifhunter"
+              />
+              <div id="tech-used" className={styles.techUsed}>
+                <div className="text-md py-2">Technologies Used</div>
+                <div className="text-sm">NextJS</div>
+                <div className="text-sm">Typescript</div>
+                <div className="text-sm">Fetch API</div>
+              </div>
+            </div>
           </li>
         </Link>
         <Link href="/">
           <li>
-            <Image
-              src="/images/spotify-artist-collage-screen.png"
-              width={2000}
-              height={1000}
-              alt="spotify-artist-collage"
-            />
+            <div>
+              <div id="title" className={styles.title}>
+                <div className="text-xl">
+                  Spotify Artist Collage - Your Top 50 Artists
+                </div>
+                <Image
+                  src="/github.svg"
+                  width={24}
+                  height={24}
+                  alt="github-link"
+                />
+              </div>
+              <Image
+                src="/images/spotify-artist-collage-screen.png"
+                width={2000}
+                height={1000}
+                alt="spotify-artist-collage"
+              />
+              <div id="tech-used" className={styles.techUsed}>
+                <div className="text-md py-2">Technologies Used</div>
+                <div className="text-sm">CSS Animations</div>
+                <div className="text-sm">OAuth</div>
+                <div className="text-sm">Server Actions</div>
+                <div className="text-sm">PostgresDB</div>
+              </div>
+            </div>
           </li>
         </Link>
       </ul>
