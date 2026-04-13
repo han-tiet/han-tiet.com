0e12cf106ccf34b0704b8923cd079f9c085fa48f
Han Tiet <hanweitiet@gmail.com>
2026-04-13 12:49:32 +0100

    feat: added hint for scrolling coverflow

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,6 +6,9 @@ export default function Projects() {
     <>
       <NavigationBar />
       <CoverFlow />
+      <div className="flex justify-center p-[3%] text-sm text-gray-400">
+        Scroll for more projects
+      </div>
     </>
   );
 }
