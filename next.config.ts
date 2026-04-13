7f9d9c27182d1cb8227e487f6bcb5efae2606149
Han Tiet <hanweitiet@gmail.com>
2026-04-13 12:57:09 +0100

    build: added allowed URLs for artist collage

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,17 @@ const nextConfig: NextConfig = {
 };
 
 module.exports = {
-  allowedDevOrigins: ["192.168.50.88", "*192.168.50.88"],
+  allowedDevOrigins: [
+    "192.168.50.121",
+    "*192.168.50.121",
+    "i.scdn.co",
+    "*i.scdn.co",
+  ],
   images: {
-    remotePatterns: [new URL("https://i.giphy.com/**")],
+    remotePatterns: [
+      new URL("https://i.giphy.com/**"),
+      new URL("https://i.scdn.co/image/**"),
+    ],
   },
   typescript: {
     // !! WARN !!
