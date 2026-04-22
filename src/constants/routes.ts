9de177dc471d25221e773badfdcab9c62261f1e8
Han Tiet <hanweitiet@gmail.com>
2026-04-22 16:30:32 +0100

    style: formatted by prettier

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -3,5 +3,5 @@ export const ROUTES = {
    * Login route for the Spotify Artist Collage login.
    */
   SPOTIFY_ARTIST_COLLAGE__LOGIN: "/projects/spotify-artist-collage/login",
-  SPOTIFY_ARTIST_COLLAGE__AUTH_API: "/projects/spotify-artist-collage/api/auth"
-}
+  SPOTIFY_ARTIST_COLLAGE__AUTH_API: "/projects/spotify-artist-collage/api/auth",
+};
