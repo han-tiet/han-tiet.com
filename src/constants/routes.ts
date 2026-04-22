d0b00aac4e05eed7b2f7f783d6ba6017dff8322d
Han Tiet <hanweitiet@gmail.com>
2026-04-22 16:28:07 +0100

    feat: initial commit

---
+++ src/constants/routes.ts
@@ -0,0 +1,7 @@
+export const ROUTES = {
+  /**
+   * Login route for the Spotify Artist Collage login.
+   */
+  SPOTIFY_ARTIST_COLLAGE__LOGIN: "/projects/spotify-artist-collage/login",
+  SPOTIFY_ARTIST_COLLAGE__AUTH_API: "/projects/spotify-artist-collage/api/auth"
+}
