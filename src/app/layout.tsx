5241c1d34fde077278aa88349b6b777880f1b66e
Han Tiet <hanweitiet@gmail.com>
2026-05-06 18:09:32 +0100

    chore: removed ThemeProvider import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "@/app/globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
 import { Inter } from "next/font/google";
 
 const inter = Inter({
