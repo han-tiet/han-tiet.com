facdf6d95147c9bbd748b3a8c1db802447c5ad5d
Han Tiet <hanweitiet@gmail.com>
2026-05-06 21:44:54 +0100

    feat: intial commit

---
+++ src/components/IndexNavigationBar.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+import NavButton from "./NavButton";
+
+export default function NavigationBar() {
+  return (
+    <div className="col-span-full grid grid-cols-2 items-center h-[14vh] px-[3vw]">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem></NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <div className="flex flex-row justify-self-end gap-4">
+        <NavButton href="/about">About</NavButton>
+        <NavButton href="/projects">Projects</NavButton>
+        <NavButton href="/contact">Contact</NavButton>
+      </div>
+    </div>
+  );
+}
