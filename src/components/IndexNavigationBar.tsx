3db3b1b640b28365b2d2d18ee6e58c3450b21a8a
Han Tiet <hanweitiet@gmail.com>
2026-05-20 10:55:12 +0100

    refactor: removed empty navbar components

--- src/components/IndexNavigationBar.tsx
+++ src/components/IndexNavigationBar.tsx
@@ -1,19 +1,12 @@
 "use client";
 
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuList,
-} from "@/components/ui/navigation-menu";
+import { NavigationMenu } from "@/components/ui/navigation-menu";
 import NavButton from "./NavButton";
 
 export default function NavigationBar() {
   return (
     <div className="col-span-full grid grid-cols-2 items-center h-[14vh] px-[3vw]">
       <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem></NavigationMenuItem>
-        </NavigationMenuList>
       </NavigationMenu>
       <div className="flex flex-row justify-self-end gap-4">
         <NavButton href="/projects">Projects</NavButton>
