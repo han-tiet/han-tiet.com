279d2af90cc1bdcae5bdf4c93e4f6ebfa22a8e82
Han Tiet <hanweitiet@gmail.com>
2026-05-20 10:57:39 +0100

    refactor: refactored empty NavigationMenu

--- src/components/IndexNavigationBar.tsx
+++ src/components/IndexNavigationBar.tsx
@@ -6,8 +6,7 @@ import NavButton from "./NavButton";
 export default function NavigationBar() {
   return (
     <div className="col-span-full grid grid-cols-2 items-center h-[14vh] px-[3vw]">
-      <NavigationMenu>
-      </NavigationMenu>
+      <NavigationMenu />
       <div className="flex flex-row justify-self-end gap-4">
         <NavButton href="/projects">Projects</NavButton>
         <NavButton href="/contact">Contact</NavButton>
