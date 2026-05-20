759d71d5954c979ff1a4aceac52c0e78890771a9
Han Tiet <hanweitiet@gmail.com>
2026-05-20 12:50:00 +0100

    refactor: removed references to barbajs

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,11 +5,9 @@ export default function Contact() {
   return (
     <div>
       <NavigationBar />
-      <main data-barba="container" data-barba-namespace="contact">
-        <div className="pt-[24px] flex justify-center h-full">
-          <ContactForm />
-        </div>
-      </main>
+      <div className="pt-[24px] flex justify-center h-full">
+        <ContactForm />
+      </div>
     </div>
   );
 }
