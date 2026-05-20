025ce41e4339bdf6e9c979289d085c427e509ac6
Han Tiet <hanweitiet@gmail.com>
2026-05-20 11:35:39 +0100

    feat: removed unneeded navbar components

--- src/components/IndexNavigationBar.tsx
+++ src/components/IndexNavigationBar.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import { NavigationMenu } from "@/components/ui/navigation-menu";
 import NavButton from "./NavButton";
 
 export default function NavigationBar() {
   return (
     <div className="col-span-full grid grid-cols-2 items-center h-[14vh] px-[3vw]">
-      <NavigationMenu />
+      <div />
       <div className="flex flex-row justify-self-end gap-4">
         <NavButton href="/projects">Projects</NavButton>
         <NavButton href="/contact">Contact</NavButton>
