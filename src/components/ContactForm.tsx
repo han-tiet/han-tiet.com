bb8f6cc8b12c48fdfde79cfddbecacc8c94ee9f7
Han Tiet <hanweitiet@gmail.com>
2026-06-01 11:47:39 +0100

    feat: replaced react form hook with server action for handling

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Controller, useForm } from "react-hook-form";
-import * as z from "zod";
-import Form from "next/form";
+
+import { useActionState } from "react"
 import {
   Field,
   FieldDescription,
@@ -13,158 +11,76 @@ import {
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import SubmitButton from "@/components/SubmitButton";
+import { Button } from "@/components/ui/button";
+import { sendEmail } from "@/app/actions/sendEmail";
 
-const formSchema = z.object({
-  forename: z
-    .string()
-    .min(1, "forename must not be empty")
-    .max(30, "forename must not have more than 30 characters"),
-  surname: z
-    .string()
-    .min(1, "Surname must not be empty")
-    .max(30, "Surname must not have more than 30 characters"),
-  emailAddress: z.email(),
-  subject: z
-    .string()
-    .min(1, "Subject must not be empty")
-    .max(50, "Surname must not have more than 50 characters"),
-  message: z
-    .string()
-    .min(1, "Message must not be empty")
-    .max(5000, "Message is too long"),
-});
+const initialState = { success: false, errors: {} }
+
 
 export default function ContactForm() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      forename: "",
-      surname: "",
-      emailAddress: "",
-      subject: "",
-      message: "",
-    },
-  });
-
-  function onSubmit(email: z.infer<typeof formSchema>) {
-    console.log(email);
-  }
+  const [state, formAction, isPending] = useActionState(sendEmail, initialState)
 
   return (
-    <div className="flex-col justify-center w-[588px]">
-      <div className="flex justify-center pb-[20px] text-[32px] font-semibold">
-        Let&apos;s have a chat
-      </div>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
-        <FieldGroup>
-          <div className="grid grid-cols-2 gap-16">
-            <Controller
-              name="forename"
-              control={form.control}
-              render={({ field, fieldState }) => (
-                <Field data-invalid={fieldState.invalid}>
-                  <Input
-                    {...field}
-                    id="forename"
-                    aria-invalid={fieldState.invalid}
-                    type="input"
-                    placeholder="Forename"
-                    className="border-[2px] rounded-[8px]"
-                    autoComplete="off"
-                  />
-                  {fieldState.invalid && (
-                    <FieldError errors={[fieldState.error]} />
-                  )}
-                </Field>
-              )}
-            />
-            <Controller
-              name="surname"
-              control={form.control}
-              render={({ field, fieldState }) => (
-                <Field data-invalid={fieldState.invalid}>
-                  <Input
-                    {...field}
-                    id="surename"
-                    aria-invalid={fieldState.invalid}
-                    type="input"
-                    placeholder="Surname"
-                    className="border-[2px] rounded-[8px]"
-                    autoComplete="off"
-                  />
-                  {fieldState.invalid && (
-                    <FieldError errors={[fieldState.error]} />
-                  )}
-                </Field>
-              )}
-            />
-          </div>
-          <Controller
+    <form action={formAction}>
+      <FieldGroup>
+        <Field>
+          <Input
+            id="forename"
+            name="forename"
+            aria-invalid="false"
+            type="input"
+            placeholder="Forename"
+            className="border-[2px] rounded-[8px]"
+            autoComplete="off"
+          />
+        </Field>
+        <Field>
+          <Input
+            id="surname"
+            name="surname"
+            aria-invalid="false"
+            type="input"
+            placeholder="Surname"
+            className="border-[2px] rounded-[8px]"
+            autoComplete="off"
+          />
+        </Field>
+        <Field>
+          <Input
+            id="emailAddress"
             name="emailAddress"
-            control={form.control}
-            render={({ field, fieldState }) => (
-              <Field data-invalid={fieldState.invalid}>
-                <Input
-                  {...field}
-                  id="email-address"
-                  aria-invalid={fieldState.invalid}
-                  type="input"
-                  placeholder="Email Address"
-                  className="border-[2px] rounded-[8px]"
-                  autoComplete="off"
-                />
-                {fieldState.invalid && (
-                  <FieldError errors={[fieldState.error]} />
-                )}
-              </Field>
-            )}
+            aria-invalid="false"
+            type="input"
+            placeholder="Email Address"
+            className="border-[2px] rounded-[8px]"
+            autoComplete="off"
           />
-          <Controller
+        </Field>
+        <Field>
+          <Input
+            id="subject"
             name="subject"
-            control={form.control}
-            render={({ field, fieldState }) => (
-              <Field data-invalid={fieldState.invalid}>
-                <Input
-                  {...field}
-                  id="subject"
-                  aria-invalid={fieldState.invalid}
-                  type="input"
-                  placeholder="Subject"
-                  className="border-[2px] rounded-[8px]"
-                  autoComplete="off"
-                />
-                {fieldState.invalid && (
-                  <FieldError errors={[fieldState.error]} />
-                )}
-              </Field>
-            )}
+            aria-invalid="false"
+            type="input"
+            placeholder="Subject"
+            className="border-[2px] rounded-[8px]"
+            autoComplete="off"
           />
-          <Controller
-            name="subject"
-            control={form.control}
-            render={({ fieldState }) => (
-              <Field data-invalid={fieldState.invalid}>
-                <Textarea
-                  id="message"
-                  aria-invalid={fieldState.invalid}
-                  placeholder="Write your message here"
-                  className="h-[300px] border-[2px] rounded-[8px]"
-                  autoComplete="off"
-                />
-                {fieldState.invalid && (
-                  <FieldError errors={[fieldState.error]} />
-                )}
-              </Field>
-            )}
+        </Field>
+        <Field>
+          <Textarea
+            id="message"
+            name="message"
+            aria-invalid="false"
+            placeholder="Write your message here"
+            className="h-[300px] border-[2px] rounded-[8px]"
+            autoComplete="off"
           />
-          <Field>
-            <div className="flex justify-center p-8">
-              <SubmitButton />
-            </div>
-          </Field>
-        </FieldGroup>
-      </form>
-    </div>
-  );
+        </Field>
+      </FieldGroup>
+      <Button type="submit">
+        Submit
+      </Button>
+    </form>
+  )
 }
