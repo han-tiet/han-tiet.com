02dcf021e9c5532813d8355b7a1806d187dea7b1
Han Tiet <hanweitiet@gmail.com>
2026-06-01 11:49:48 +0100

    feat: separated title from contact form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,7 @@
+import * as React from "react";
+
+import { redirect } from "next/navigation";
+
 import NavigationBar from "@/components/NavigationBar";
 import ContactForm from "@/components/ContactForm";
 
@@ -6,7 +10,12 @@ export default function Contact() {
     <div>
       <NavigationBar />
       <div className="pt-[24px] flex justify-center h-full">
-        <ContactForm />
+        <div className="flex-col justify-center w-[588px]">
+          <div className="flex justify-center pb-[20px] text-[32px] font-semibold">
+            Let&apos;s have a chat
+          </div>
+          <ContactForm />
+        </div>
       </div>
     </div>
   );
