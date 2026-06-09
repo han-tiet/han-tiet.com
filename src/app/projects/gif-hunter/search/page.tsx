f50ce7b07e165bbccd366311231cc4b4ec7f8b85
Han Tiet <hanweitiet@gmail.com>
2026-06-09 17:34:19 +0100

    style: formatted by prettier

--- src/app/projects/gif-hunter/search/page.tsx
+++ src/app/projects/gif-hunter/search/page.tsx
@@ -12,28 +12,32 @@ export default async function resultsPage({
   const { p } = await searchParams;
 
   async function fetchResults1() {
-    const resp = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.GH_API_KEY_1}&q=${p}&limit=10&offset=0&rating=r&lang=en&bundle=messaging_non_clips`)
-    const json = await resp.json()
-    return json.data
+    const resp = await fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=${process.env.GH_API_KEY_1}&q=${p}&limit=10&offset=0&rating=r&lang=en&bundle=messaging_non_clips`,
+    );
+    const json = await resp.json();
+    return json.data;
   }
 
-  async function fetchResults2() { 
-    const resp = await fetch(`https://api.klipy.com/api/v1/${process.env.GH_API_KEY_2}/gifs/search?page=1&per_page=24&q=${p}&customer_id=guest&locale=uk&content_filter=off`)
-    const json = await resp.json()
-    return json.data.data
+  async function fetchResults2() {
+    const resp = await fetch(
+      `https://api.klipy.com/api/v1/${process.env.GH_API_KEY_2}/gifs/search?page=1&per_page=24&q=${p}&customer_id=guest&locale=uk&content_filter=off`,
+    );
+    const json = await resp.json();
+    return json.data.data;
   }
 
   const [results1, results2] = await Promise.allSettled([
     fetchResults1(),
     fetchResults2(),
-  ])
+  ]);
 
   if (results1.status === "rejected" && results2.status === "rejected") {
-    return notFound()
-  } 
+    return notFound();
+  }
 
-  const gifs1 = results1.status === "fulfilled" ? results1.value : []
-  const gifs2 = results2.status === "fulfilled" ? results2.value : []
+  const gifs1 = results1.status === "fulfilled" ? results1.value : [];
+  const gifs2 = results2.status === "fulfilled" ? results2.value : [];
 
   return (
     <Box>
