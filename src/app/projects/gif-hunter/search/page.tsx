c82da5474dbd4a075562f26d5ecfa36b66ffb274
Han Tiet <hanweitiet@gmail.com>
2026-06-09 17:33:53 +0100

    refactor: refactored to function during single API failure

--- src/app/projects/gif-hunter/search/page.tsx
+++ src/app/projects/gif-hunter/search/page.tsx
@@ -10,20 +10,30 @@ export default async function resultsPage({
   searchParams: Promise<{ p: string }>;
 }) {
   const { p } = await searchParams;
-  // console.log(p)
 
-  const resp1 = await fetch(
-    `https://api.giphy.com/v1/gifs/search?api_key=${process.env.GH_API_KEY_1}&q=${p}&limit=10&offset=0&rating=r&lang=en&bundle=messaging_non_clips`,
-  );
-  const resp2 = await fetch(
-    `https://api.klipy.com/api/v1/${process.env.GH_API_KEY_2}/gifs/search?page=1&per_page=24&q=${p}&customer_id=guest&locale=uk&content_filter=off`,
-  );
-
-  if (!resp1.ok || !resp2.ok) {
-    return notFound();
+  async function fetchResults1() {
+    const resp = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.GH_API_KEY_1}&q=${p}&limit=10&offset=0&rating=r&lang=en&bundle=messaging_non_clips`)
+    const json = await resp.json()
+    return json.data
   }
 
-  const respJson = await Promise.all([resp1.json(), resp2.json()]);
+  async function fetchResults2() { 
+    const resp = await fetch(`https://api.klipy.com/api/v1/${process.env.GH_API_KEY_2}/gifs/search?page=1&per_page=24&q=${p}&customer_id=guest&locale=uk&content_filter=off`)
+    const json = await resp.json()
+    return json.data.data
+  }
+
+  const [results1, results2] = await Promise.allSettled([
+    fetchResults1(),
+    fetchResults2(),
+  ])
+
+  if (results1.status === "rejected" && results2.status === "rejected") {
+    return notFound()
+  } 
+
+  const gifs1 = results1.status === "fulfilled" ? results1.value : []
+  const gifs2 = results2.status === "fulfilled" ? results2.value : []
 
   return (
     <Box>
@@ -31,14 +41,14 @@ export default async function resultsPage({
         <Typography
           variant="h3"
           component="a"
-          href="/gif-hunter"
+          href="/projects/gif-hunter"
           sx={{ textDecoration: "none", color: "black" }}
         >
           GIFHunter
         </Typography>
       </Box>
       <Search />
-      <Results source_1={respJson[0].data} source_2={respJson[1].data.data} />
+      <Results source_1={gifs1} source_2={gifs2} />
     </Box>
   );
 }
