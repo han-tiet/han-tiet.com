5f0497dfd25a731ad91992458c3d3086a719daf2
Han Tiet <hanweitiet@gmail.com>
2026-06-10 16:32:45 +0100

    feat: added loading animation

--- src/features/gif-hunter/components/Results.tsx
+++ src/features/gif-hunter/components/Results.tsx
@@ -1,4 +1,6 @@
 "use client";
+
+import { useState, useCallback, useEffect } from "react"
 import React from "react";
 import Box from "@mui/material/Box";
 import Masonry from "@mui/lab/Masonry";
@@ -9,15 +11,30 @@ interface Props {
 }
 
 const Results: React.FC<Props> = (props) => {
+  const [loadedCount, setLoadedCount] = useState(0)
+  const totalCount = props.source_1.length + props.source_2.length
+  const allLoaded = loadedCount >= totalCount
+
+  const handleLoad = useCallback(() => {
+    setLoadedCount(prev => prev + 1)
+  }, [])
+
+  useEffect(() => {
+    setLoadedCount(0)
+  }, [props])
+
   return (
     <Box sx={{ width: 900, minHeight: 829, m: "auto" }}>
-      <Masonry columns={3} spacing={2}>
+      {!allLoaded && (<div className="flex justify-center">Loading...</div>)}
+      <Masonry columns={3} spacing={2} style={{visibility: allLoaded ? "visible" : "hidden"}}>
         {props.source_1.map((source_1: React.ReactNode, index: number) => (
           <div key={index}>
             <img
               src={`https://i.giphy.com/${source_1.id}.webp`}
               alt={source_1.title}
               loading="lazy"
+              onLoad={handleLoad}
+              onError={handleLoad}
               style={{
                 borderBottomLeftRadius: 4,
                 borderBottomRightRadius: 4,
@@ -33,6 +50,8 @@ const Results: React.FC<Props> = (props) => {
               src={source_2.file.hd.gif.url}
               alt={source_2.title}
               loading="lazy"
+              onLoad={handleLoad}
+              onError={handleLoad}
               style={{
                 borderBottomLeftRadius: 4,
                 borderBottomRightRadius: 4,
