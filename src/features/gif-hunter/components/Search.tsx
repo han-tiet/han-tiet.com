392b078a48f7036248e0d4732ea053484fcb64ec
Han Tiet <hanweitiet@gmail.com>
2026-06-11 16:39:41 +0100

    feat: made search bar text persist on submit

--- src/features/gif-hunter/components/Search.tsx
+++ src/features/gif-hunter/components/Search.tsx
@@ -1,9 +1,18 @@
 "use client";
-import React from "react";
+
+import { useState, useEffect } from "react";
 import { Box, TextField } from "@mui/material";
 import Form from "next/form";
+import { useSearchParams } from "next/navigation";
 
 export default function Search() {
+  const searchParams = useSearchParams();
+  const [value, setValue] = useState(searchParams.get("p") ?? "");
+
+  useEffect(() => {
+    setValue(searchParams.get("p") ?? "");
+  }, [searchParams]);
+
   return (
     <Box sx={{ width: "30rem", my: 5, mx: "auto" }}>
       <Form action="/projects/gif-hunter/search">
@@ -15,10 +24,12 @@ export default function Search() {
           }}
         >
           <TextField
-            id="outlined-basic"
+            value={value}
+            id="search-bar"
             label="Search for a GIF here"
             variant="outlined"
             name="p"
+            onChange={(e) => setValue(e.target.value)}
             fullWidth
           />
         </Box>
