a333b0261518c3a31ff4e5f11bee49d36c7714cc
Han Tiet <hanweitiet@gmail.com>
2026-06-11 16:30:09 +0100

    feat: added cache provider for MUI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "@/app/globals.css";
 import { Inter } from "next/font/google";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v16-appRouter"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -27,7 +28,11 @@ export default function RootLayout({
         <meta httpEquiv="X-UA-Compatible" content="ie=edge"></meta>
         <title>Han Tiet&apos;s Portfolio</title>
       </head>
-      <body className="h-screen w-screen overflow-x-hidden">{children}</body>
+      <body className="h-screen w-screen overflow-x-hidden">
+        <AppRouterCacheProvider>
+          {children}
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
