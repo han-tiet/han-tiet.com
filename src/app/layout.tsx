efbf0040d96e51c3789fb1c1e43c1114a463d7cd
Han Tiet <hanweitiet@gmail.com>
2026-06-30 16:24:13 +0100

    feat: removed end tag from Toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,8 @@ export default function RootLayout({
       </head>
       <body className="h-screen w-screen overflow-x-hidden">
         <AppRouterCacheProvider>
-          <Toaster position="bottom-right">{children}</Toaster>
+          <Toaster position="bottom-right" />
+          {children}
         </AppRouterCacheProvider>
       </body>
     </html>
