f52d793c9850870164fc1049ca78d78969c1f492
Han Tiet <hanweitiet@gmail.com>
2026-07-06 09:57:08 +0100

    build: wrapped page in react fragment

--- src/app/projects/gif-hunter/page.tsx
+++ src/app/projects/gif-hunter/page.tsx
@@ -3,21 +3,23 @@ import { Box, Typography } from "@mui/material";
 
 export default async function GifHunter() {
   return (
-    <Box sx={{ display: "flex-col", alignItems: "center", paddingY: "30vh" }}>
-      <Typography
-        variant="h1"
-        component="a"
-        href="/projects/gif-hunter"
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          textDecoration: "none",
-          color: "black",
-        }}
-      >
-        GIFHunter
-      </Typography>
-      <IndexSearchBar />
-    </Box>
+    <>
+      <Box sx={{ display: "flex-col", alignItems: "center", paddingY: "30vh" }}>
+        <Typography
+          variant="h1"
+          component="a"
+          href="/projects/gif-hunter"
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            textDecoration: "none",
+            color: "black",
+          }}
+        >
+          GIFHunter
+        </Typography>
+        <IndexSearchBar />
+      </Box>
+    </>
   );
 }
