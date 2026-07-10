bff1bf5140faec3a1f7f4e7dca2a2744b59174e7
Han Tiet <hanweitiet@gmail.com>
2026-07-10 17:59:54 +0100

    feat: added bounce and complaint handling

--- src/app/api/ses-notifications/route.ts
+++ src/app/api/ses-notifications/route.ts
@@ -1,32 +1,57 @@
-"use server";
-
 import { NextResponse } from "next/server";
 import crypto from "crypto";
+import { PutSuppressedDestinationCommand } from "@aws-sdk/client-sesv2";
 
-export async function handleSESNotification(request: Request) {
+export async function POST(request: Request) {
   try {
     const payload = await request.json();
+    const resp = await fetch(payload.SigningCertURL);
+    const code = await resp.text();
 
-    if (payload.type === "SubscriptionConfirmation" || "Notification") {
-      const verify = crypto.createVerify(payload.signingCertURL);
+    const verify = crypto.createVerify("sha1");
+    verify.update(code);
 
-      if (verify) {
-        return NextResponse.json({ response: "Success" }, { status: 200 });
-      } else {
-        return NextResponse.json(
-          { response: "400 Bad Request" },
-          { status: 400 },
-        );
-      }
-    } else {
+    if (verify.verify(code, payload.Signature) !== true) {
       return NextResponse.json(
         { response: "400 Bad Request" },
         { status: 400 },
       );
     }
+
+    if (payload.Type === "SubscriptionConfirmation") {
+      const resp = await fetch(payload.SubscribeURL);
+
+      return NextResponse.json({ response: "Success" }, { status: 200 });
+    }
+
+    if (payload.Type === "Notification") {
+      const message = JSON.parse(payload.Message);
+
+      if (message.notificationType == "Complaint") {
+        const complainedRecipients = message.complaint.complainedRecipients;
+
+        for (let i = 0; i < complainedRecipients.length; i++) {
+          new PutSuppressedDestinationCommand({
+            EmailAddress: complainedRecipients[i],
+            Reason: "COMPLAINT",
+          });
+        }
+      }
+
+      if (message.notificationType == "Bounce") {
+        const bouncedRecipients = message.bounce.bouncedRecipients;
+
+        for (let i = 0; i < bouncedRecipients.length; i++) {
+          new PutSuppressedDestinationCommand({
+            EmailAddress: bouncedRecipients[i],
+            Reason: "BOUNCE",
+          });
+        }
+      }
+    }
   } catch {
     return NextResponse.json(
-      { error: "500Internal Server Error" },
+      { error: "500 Internal Server Error" },
       { status: 500 },
     );
   }
