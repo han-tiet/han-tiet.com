116677ca1dcfcdfd3dd194c2712cee0585e40d46
Han Tiet <hanweitiet@gmail.com>
2026-07-13 16:28:00 +0100

    feat: improved bounce and complaint handling

--- src/app/api/ses-notifications/route.ts
+++ src/app/api/ses-notifications/route.ts
@@ -1,53 +1,160 @@
 import { NextResponse } from "next/server";
 import crypto from "crypto";
-import { PutSuppressedDestinationCommand } from "@aws-sdk/client-sesv2";
+import {
+  SESv2Client,
+  PutSuppressedDestinationCommand,
+} from "@aws-sdk/client-sesv2";
+import { email } from "better-auth";
+
+const config = { region: process.env.AWS_REGION };
+const client = new SESv2Client(config);
 
 export async function POST(request: Request) {
   try {
     const payload = await request.json();
-    const resp = await fetch(payload.SigningCertURL);
-    const code = await resp.text();
 
-    const verify = crypto.createVerify("sha1");
-    verify.update(code);
+    // Validate SigningCertURL hostname
 
-    if (verify.verify(code, payload.Signature) !== true) {
+    const signingCertURL = new URL(payload.SigningCertURL);
+
+    if (!signingCertURL.hostname.endsWith(".amazonaws.com")) {
       return NextResponse.json(
         { response: "400 Bad Request" },
         { status: 400 },
       );
     }
 
-    if (payload.Type === "SubscriptionConfirmation") {
-      const resp = await fetch(payload.SubscribeURL);
+    const resp = await fetch(payload.SigningCertURL);
+    const code = await resp.text();
 
-      return NextResponse.json({ response: "Success" }, { status: 200 });
-    }
+    const verify = crypto.createVerify("sha1");
 
     if (payload.Type === "Notification") {
+      // Feed string of payload fields into verifier
+
+      verify.update(
+        `Message
+${payload.Message}
+MessageId
+${payload.MessageId}
+Timestamp
+${payload.Timestamp}
+TopicArn
+${payload.TopicArn}
+Type
+${payload.Type}
+`,
+      );
+
+      // Fetch PEM certificate and verify against signature from payload
+
+      if (
+        verify.verify(code, Buffer.from(payload.Signature, "base64")) == false
+      ) {
+        return NextResponse.json(
+          { response: "400 Bad Request" },
+          { status: 400 },
+        );
+      }
+
       const message = JSON.parse(payload.Message);
 
       if (message.notificationType == "Complaint") {
         const complainedRecipients = message.complaint.complainedRecipients;
 
         for (let i = 0; i < complainedRecipients.length; i++) {
-          new PutSuppressedDestinationCommand({
-            EmailAddress: complainedRecipients[i],
+          const command = new PutSuppressedDestinationCommand({
+            EmailAddress: complainedRecipients[i].emailAddress,
             Reason: "COMPLAINT",
           });
+
+          await client.send(command);
         }
       }
 
       if (message.notificationType == "Bounce") {
-        const bouncedRecipients = message.bounce.bouncedRecipients;
+        if (message.bounce.bounceType == "Permanent") {
+          const bouncedRecipients = message.bounce.bouncedRecipients;
 
-        for (let i = 0; i < bouncedRecipients.length; i++) {
-          new PutSuppressedDestinationCommand({
-            EmailAddress: bouncedRecipients[i],
-            Reason: "BOUNCE",
-          });
+          for (let i = 0; i < bouncedRecipients.length; i++) {
+            const command = new PutSuppressedDestinationCommand({
+              EmailAddress: bouncedRecipients[i].emailAddress,
+              Reason: "BOUNCE",
+            });
+
+            await client.send(command);
+          }
+        } else {
+          const bouncedRecipients = message.bounce.bouncedRecipients;
+
+          for (let i = 0; i < bouncedRecipients.length; i++) {
+            const emailAddress = bouncedRecipients[i].emailAddress.split("@");
+
+            console.warn({
+              bounceType: message.bounce.bounceType,
+              bounceSubType: message.bounce.bounceSubType,
+              emailAddress: emailAddress[1],
+              diagnosticCode: bouncedRecipients[i].diagnosticCode,
+              action: bouncedRecipients[i].action,
+              messageId: payload.MessageId,
+              originalMessageId: message.mail.messageId,
+            });
+          }
         }
       }
+      return NextResponse.json({ response: "200 Success" }, { status: 200 });
+    }
+
+    if (payload.Type === "SubscriptionConfirmation") {
+      verify.update(
+        `Message
+${payload.Message}
+MessageId
+${payload.MessageId}
+SubscribeURL
+${payload.SubscribeURL}
+Timestamp
+${payload.Timestamp}
+Token
+${payload.Token}
+TopicArn
+${payload.TopicArn}
+Type
+${payload.Type}
+`,
+      );
+
+      if (
+        verify.verify(code, Buffer.from(payload.Signature, "base64")) == false
+      ) {
+        return NextResponse.json(
+          { response: "400 Bad Request" },
+          { status: 400 },
+        );
+      }
+      const resp = await fetch(payload.SubscribeURL);
+
+      if (!resp.ok) {
+        console.error({
+          message: "Failed to confirm SNS subscription",
+          subscribeURL: payload.SubscribeURL,
+          status: resp.status,
+        });
+        return NextResponse.json(
+          { response: "500 Internal Sever Error" },
+          { status: 500 },
+        );
+      }
+
+      return NextResponse.json({ response: "200 Success" }, { status: 200 });
+    }
+
+    if (
+      payload.Type != "Notification" &&
+      payload.Type != "SubscriptionConfirmation"
+    ) {
+      console.warn("200 Unrecognised Message Type");
+      return NextResponse.json({ response: "200 Success" }, { status: 200 });
     }
   } catch {
     return NextResponse.json(
