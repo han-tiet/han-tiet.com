c09b20cbd4e61f9c6316f1840f91ea458c7cc07c
Han Tiet <hanweitiet@gmail.com>
2026-07-16 17:16:43 +0100

    feat: added SES message types

--- src/lib/ses/types.ts
+++ src/lib/ses/types.ts
@@ -9,3 +9,30 @@ export type SNSPayload = {
   SubscribeURL?: string;
   Token?: string;
 };
+
+export type SESComplaintMessage = {
+  notificationType: "Complaint";
+  complaint: {
+    complainedRecipients: { emailAddress: string }[];
+    bounceType?: never;
+  };
+  mail: {
+    messageId: string;
+  };
+};
+
+export type SESBounceMessage = {
+  notificationType: "Bounce";
+  bounce: {
+    bounceType: "Permanent" | "Transient";
+    bounceSubType: string;
+    bouncedRecipients: {
+      emailAddress: string;
+      diagnosticCode?: string;
+      action?: string;
+    }[];
+  };
+  mail: {
+    messageId: string;
+  };
+};
