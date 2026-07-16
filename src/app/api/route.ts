71a9a279b18122c9e6f740e7c0751a682e88b9e0
Han Tiet <hanweitiet@gmail.com>
2026-07-16 15:50:33 +0100

    refactor: refactored code based on SRP

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -8,6 +8,7 @@ import { SNSPayload } from "@/lib/ses/types";
 
 const config = { region: process.env.AWS_REGION };
 const client = new SESv2Client(config);
+const verify = crypto.createVerify("sha1");
 
 export async function POST(request: Request) {
   try {
@@ -16,119 +17,18 @@ export async function POST(request: Request) {
     // Validate SigningCertURL hostname
 
     const signingCertURL = new URL(payload.SigningCertURL);
+    validateHostname(signingCertURL);
 
-    if (!signingCertURL.hostname.endsWith(".amazonaws.com")) {
-      console.error({
-        message: "Invalid signingCertURL hostname",
-        hostname: signingCertURL.hostname,
-      });
-      return NextResponse.json(
-        { response: "400 Bad Request" },
-        { status: 400 },
-      );
-    }
-
-    const resp = await fetch(payload.SigningCertURL);
-    const code = await resp.text();
-
-    const verify = crypto.createVerify("sha1");
+    // Handle Notification
 
     if (payload.Type === "Notification") {
-      // Feed string of payload fields into verifier
-
-      verify.update(buildNotificationString(payload));
-
-      // Fetch PEM certificate and verify against signature from payload
-
-      if (
-        verify.verify(code, Buffer.from(payload.Signature, "base64")) == false
-      ) {
-        console.error({
-          message: "PEM certificate does not match payload signature",
-        });
-        return NextResponse.json(
-          { response: "400 Bad Request" },
-          { status: 400 },
-        );
-      }
-
-      const message = JSON.parse(payload.Message);
-
-      if (message.notificationType == "Complaint") {
-        const complainedRecipients = message.complaint.complainedRecipients;
-
-        for (let i = 0; i < complainedRecipients.length; i++) {
-          const command = new PutSuppressedDestinationCommand({
-            EmailAddress: complainedRecipients[i].emailAddress,
-            Reason: "COMPLAINT",
-          });
-
-          await client.send(command);
-        }
-      }
-
-      if (message.notificationType == "Bounce") {
-        if (message.bounce.bounceType == "Permanent") {
-          const bouncedRecipients = message.bounce.bouncedRecipients;
-
-          for (let i = 0; i < bouncedRecipients.length; i++) {
-            const command = new PutSuppressedDestinationCommand({
-              EmailAddress: bouncedRecipients[i].emailAddress,
-              Reason: "BOUNCE",
-            });
-
-            await client.send(command);
-          }
-        } else {
-          const bouncedRecipients = message.bounce.bouncedRecipients;
-
-          for (let i = 0; i < bouncedRecipients.length; i++) {
-            const emailAddress = bouncedRecipients[i].emailAddress.split("@");
-
-            console.warn({
-              bounceType: message.bounce.bounceType,
-              bounceSubType: message.bounce.bounceSubType,
-              emailAddress: emailAddress[1],
-              diagnosticCode: bouncedRecipients[i].diagnosticCode,
-              action: bouncedRecipients[i].action,
-              messageId: payload.MessageId,
-              originalMessageId: message.mail.messageId,
-            });
-          }
-        }
-      }
-      return NextResponse.json({ response: "200 Success" }, { status: 200 });
+      handleNotification(payload);
     }
 
+    // Handle Subscription confirmation
+
     if (payload.Type === "SubscriptionConfirmation") {
-      verify.update(buildConfirmationString(payload));
-
-      if (
-        verify.verify(code, Buffer.from(payload.Signature, "base64")) == false
-      ) {
-        console.error({
-          message: "PEM certificate does not match payload signature",
-        });
-        return NextResponse.json(
-          { response: "400 Bad Request" },
-          { status: 400 },
-        );
-      }
-      const resp = await fetch(payload.SubscribeURL);
-
-      if (!resp.ok) {
-        console.error({
-          message: "Failed to confirm SNS subscription",
-          subscribeURL: payload.SubscribeURL,
-          status: resp.status,
-        });
-        return NextResponse.json(
-          { response: "500 Internal Sever Error" },
-          { status: 500 },
-        );
-      }
-
-      return NextResponse.json({ response: "200 Success" }, { status: 200 });
+      handleConfirmation(payload);
     }
 
     if (
@@ -144,6 +44,138 @@ export async function POST(request: Request) {
       { error: "500 Internal Server Error" },
       { status: 500 },
     );
+  }
+}
+
+async function validateHostname(signingCertURL: URL) {
+  if (!signingCertURL.hostname.endsWith(".amazonaws.com")) {
+    console.error({
+      message: "Invalid signingCertURL hostname",
+      hostname: signingCertURL.hostname,
+    });
+    return NextResponse.json({ response: "400 Bad Request" }, { status: 400 });
+  }
+}
+
+async function handleNotification(payload: SNSPayload) {
+  const code = await fetchPEMCert(payload);
+
+  verifyNotification(payload, code);
+
+  const message = JSON.parse(payload.Message);
+
+  if (message.notificationType == "Complaint") {
+    handleComplaint(payload);
+  }
+
+  if (message.notificationType == "Bounce") {
+    handleBounce(payload, code);
+  }
+  return NextResponse.json({ response: "200 Success" }, { status: 200 });
+}
+
+async function handleConfirmation(payload: SNSPayload) {
+  const code = await fetchPEMCert(payload);
+
+  verifyConfirmation(payload, code);
+
+  const resp = await fetch(payload.SubscribeURL);
+
+  if (!resp.ok) {
+    console.error({
+      message: "Failed to confirm SNS subscription",
+      subscribeURL: payload.SubscribeURL,
+      status: resp.status,
+    });
+    return NextResponse.json(
+      { response: "500 Internal Sever Error" },
+      { status: 500 },
+    );
+  }
+
+  return NextResponse.json({ response: "200 Success" }, { status: 200 });
+}
+
+async function fetchPEMCert(payload: SNSPayload) {
+  // Fetch PEMCert from SigningCertURL
+
+  const resp = await fetch(payload.SigningCertURL);
+  const code = await resp.text();
+
+  return code;
+}
+
+function verifyNotification(payload: SNSPayload, code: string) {
+  // Feed string of payload fields into verifier
+
+  verify.update(buildNotificationString(payload));
+
+  // Fetch PEM certificate and verify against signature from payload
+
+  if (verify.verify(code, Buffer.from(payload.Signature, "base64")) == false) {
+    console.error({
+      message: "PEM certificate does not match payload signature",
+    });
+    return NextResponse.json({ response: "400 Bad Request" }, { status: 400 });
+  }
+}
+
+function verifyConfirmation(payload: SNSPayload, code: string) {
+  // Feed string of payload fields into verifier
+
+  verify.update(buildConfirmationString(payload));
+
+  // Fetch PEM certificate and verify against signature from payload
+
+  if (verify.verify(code, Buffer.from(payload.Signature, "base64")) == false) {
+    console.error({
+      message: "PEM certificate does not match payload signature",
+    });
+    return NextResponse.json({ response: "400 Bad Request" }, { status: 400 });
+  }
+}
+
+async function handleComplaint(message) {
+  const complainedRecipients = message.complaint.complainedRecipients;
+
+  for (let i = 0; i < complainedRecipients.length; i++) {
+    const command = new PutSuppressedDestinationCommand({
+      EmailAddress: complainedRecipients[i].emailAddress,
+      Reason: "COMPLAINT",
+    });
+
+    await client.send(command);
+  }
+}
+
+async function handleBounce(payload: SNSPayload, message) {
+  if (message.bounce.bounceType == "Permanent") {
+    const bouncedRecipients = message.bounce.bouncedRecipients;
+
+    for (let i = 0; i < bouncedRecipients.length; i++) {
+      const command = new PutSuppressedDestinationCommand({
+        EmailAddress: bouncedRecipients[i].emailAddress,
+        Reason: "BOUNCE",
+      });
+
+      await client.send(command);
+    }
+  } else {
+    const bouncedRecipients = message.bounce.bouncedRecipients;
+
+    for (let i = 0; i < bouncedRecipients.length; i++) {
+      const emailAddress = bouncedRecipients[i].emailAddress.split("@");
+
+      console.warn({
+        bounceType: message.bounce.bounceType,
+        bounceSubType: message.bounce.bounceSubType,
+        emailAddress: emailAddress[1],
+        diagnosticCode: bouncedRecipients[i].diagnosticCode,
+        action: bouncedRecipients[i].action,
+        messageId: payload.MessageId,
+        originalMessageId: message.mail.messageId,
+      });
+    }
   }
 }
 
