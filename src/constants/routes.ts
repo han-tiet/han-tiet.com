92250a26f24adc42e593fb705edb210771a5ed77
Han Tiet <hanweitiet@gmail.com>
2026-07-20 17:07:59 +0100

    feat: added route constants

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,7 +1,9 @@
 export const ROUTES = {
-  /**
-   * Login route for the Spotify Artist Collage login.
-   */
+  //* Routes for page links */
+  INDEX: "/",
+  PROJECTS: "/projects",
+  CONTACT: "/contact",
+  SPOTIFY_ARTIST_COLLAGE__INDEX: "/projects/spotify-artist-collage",
   SPOTIFY_ARTIST_COLLAGE__LOGIN: "/projects/spotify-artist-collage/login",
   SPOTIFY_ARTIST_COLLAGE__AUTH_API: "/projects/spotify-artist-collage/api/auth",
   GIFHUNTER_INDEX: "/projects/gif-hunter",
