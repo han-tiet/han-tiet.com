ff6aeffe0276537458222a3e846944ee72524e76
Han Tiet <hanweitiet@gmail.com>
2026-07-23 11:01:49 +0100

    feat: changed URLs to match dev, preview and production

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,15 +1,30 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/prisma";
+import { ROUTES } from "@/constants/routes";
 
 export const auth = betterAuth({
-  basePath: "/projects/spotify-artist-collage/api/auth",
+  baseURL: {
+    allowedHosts: [
+      `${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`,
+      `www.${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`,
+      `${process.env.BETTER_AUTH_URL}`,
+    ],
+    protocol: "https",
+    fallback: "https://han-tiet.com",
+  },
+
+  basePath: ROUTES.SPOTIFY_ARTIST_COLLAGE__AUTH_API,
 
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
 
-  trustedOrigins: process.env.TRUSTED_ORIGINS?.split(",") ?? [], // Added index to trusted origins to remove NextJS warning
+  trustedOrigins: [
+    `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`,
+    `https://*.${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`,
+    `https://${process.env.BETTER_AUTH_URL}`,
+  ], // Added index to trusted origins to remove NextJS warning
 
   socialProviders: {
     // Add provider for Spotify OAuth login page
@@ -17,7 +32,7 @@ export const auth = betterAuth({
       clientId: process.env.SPOTIFY_ID as string,
       clientSecret: process.env.SPOTIFY_SECRET as string,
       scope: ["user-top-read", "user-read-recently-played"],
-      redirectURI: `${process.env.BETTER_AUTH_URL}/projects/spotify-artist-collage/api/auth/callback/spotify`,
+      redirectURI: `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL || process.env.BETTER_AUTH_URL}/projects/spotify-artist-collage/api/auth/callback/spotify`,
     },
   },
 
