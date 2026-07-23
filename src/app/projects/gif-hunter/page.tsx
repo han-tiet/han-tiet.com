3967073ac9ae2beb8d0c465b1a96bcc5fd0b1c89
Han Tiet <hanweitiet@gmail.com>
2026-07-23 10:43:39 +0100

    refactor: changed href to route constant

--- src/app/projects/gif-hunter/page.tsx
+++ src/app/projects/gif-hunter/page.tsx
@@ -5,7 +5,19 @@ import { ROUTES } from "@/constants/routes";
 
 export default async function GifHunter() {
   return (
-    <div>
+    <Box sx={{ display: "flex-col" }}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "start",
+          alignItems: "center",
+          height: "5vh",
+          padding: "1vw",
+          "&:hover": { color: "oklch(0.6911 0.2062 41.46)" },
+        }}
+      >
+        <a href={ROUTES.PROJECTS}>&lt;&lt; Projects</a>
+      </Box>
       <Box sx={{ display: "flex-col", alignItems: "center", paddingY: "30vh" }}>
         <Typography
           variant="h1"
@@ -24,6 +36,6 @@ export default async function GifHunter() {
           <IndexSearchBar />
         </Suspense>
       </Box>
-    </div>
+    </Box>
   );
 }
--- src/app/projects/gif-hunter/search/page.tsx
+++ src/app/projects/gif-hunter/search/page.tsx
@@ -32,7 +32,7 @@ export default async function GifHunter({
           "&:hover": { color: "oklch(0.6911 0.2062 41.46)" },
         }}
       >
-        <a href={`${process.env.NEXT_PUBLIC_PROJECTS}`}>&lt;&lt; Projects</a>
+        <a href={ROUTES.PROJECTS}>&lt;&lt; Projects</a>
       </Box>
       <Typography
         variant="h3"
