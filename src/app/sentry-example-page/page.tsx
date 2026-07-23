ce1fcc871b5a962dfad7aed47d565926d190fd3e
Han Tiet <hanweitiet@gmail.com>
2026-07-23 11:51:37 +0100

    feat: changed trigger for notFound page

--- src/app/sentry-example-page/page.tsx
+++ src/app/sentry-example-page/page.tsx
@@ -4,7 +4,7 @@ import * as Sentry from "@sentry/nextjs";
 import notFound from "@/app/sentry-example-page/not-found";
 
 export default function SentryTestPage() {
-  if (process.env.VERCEL_ENV) {
+  if (process.env.VERCEL_ENV != "development") {
     return notFound();
   }
   // 1. Thrown error — tests error tracking + source maps
