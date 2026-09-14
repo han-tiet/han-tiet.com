cbd520df8738e164aed755f94967025be09da6c5
Han Tiet <hanweitiet@gmail.com>
2026-09-14 16:27:41 +0100

    feat: formatted pages for landscape tablets

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,7 +6,7 @@ export default function Contact() {
     <div>
       <NavigationBar />
       <div className="pb-[1rem] flex justify-center h-full">
-        <div className="flex-col justify-center w-[100vw]">
+        <div className="flex-col justify-center w-[100%] md:w-[90%] lg:w-[40%] [@media(max-height:500px)_and_(orientation:landscape)]:w-[60%]">
           <div className="flex justify-center pb-[1rem] text-[1.25rem] font-semibold">
             Let&apos;s have a chat
           </div>
