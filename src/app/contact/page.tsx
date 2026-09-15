df5e6856191796a453593522ad5d708c02f55e45
Han Tiet <hanweitiet@gmail.com>
2026-09-15 10:35:08 +0100

    feat: scaled components for laptops and desktops

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,8 +6,8 @@ export default function Contact() {
     <div>
       <NavigationBar />
       <div className="pb-[1rem] flex justify-center h-full">
-        <div className="flex-col justify-center w-[100%] md:w-[90%] lg:w-[40%] [@media(max-height:500px)_and_(orientation:landscape)]:w-[60%]">
-          <div className="flex justify-center pb-[1rem] text-[1.25rem] font-semibold">
+        <div className="flex-col justify-center w-[100%] md:w-[70%] lg:w-[60%] lg:desktop:w-[40%] [@media(max-height:500px)_and_(orientation:landscape)]:w-[60%]">
+          <div className="flex justify-center pb-[1rem] desktop:pb-[2rem] text-[1.25rem] desktop:text-[2.5rem] font-semibold">
             Let&apos;s have a chat
           </div>
           <ContactForm />
