4fd443d8993eacaa77bd6737da92cb4669ea4357
Han Tiet <hanweitiet@gmail.com>
2026-09-15 16:09:46 +0100

    feat: reduced padding around project link for desktops

--- src/app/projects/spotify-artist-collage/login/page.tsx
+++ src/app/projects/spotify-artist-collage/login/page.tsx
@@ -5,16 +5,16 @@ import { ROUTES } from "@/constants/routes";
 
 export default function LoginPage() {
   return (
-    <div className="grid grid-col-12 min-h-[65vh]">
-      <div className="flex justify-start items-center min-h-[5vh] px-4 hover:text-primary">
+    <div className="grid grid-col-12 min-h-[55vh] desktop:min-h-[45vh]">
+      <div className="flex justify-start items-center min-h-[5rem] px-4 text-sm md:text-lg desktop:text-xl hover:text-primary">
         <a href={ROUTES.PROJECTS}>&lt;&lt; Projects</a>
       </div>
-      <div className="flex flex-col justify-center items-center gap-8 px-6 py-12 text-center">
-        <div className="flex flex-col justify-center items-center gap-4">
-          <div className="text-3xl sm:text-5xl md:text-7xl font-bold">
+      <div className="flex flex-col justify-center items-center gap-8 desktop:gap-12 px-6 desktop:px-9 py-12 desktop:pt-108 text-center">
+        <div className="flex flex-col justify-center items-center gap-4 desktop:gap-6">
+          <div className="text-3xl sm:text-5xl md:text-7xl desktop:text-[6.75rem] font-bold">
             Spotify Artist Collage
           </div>
-          <div className="text-base sm:text-lg">
+          <div className="text-base sm:text-lg desktop:text-[1.75rem]">
             Your favourite artists, like you&apos;ve never seen them before
           </div>
         </div>
