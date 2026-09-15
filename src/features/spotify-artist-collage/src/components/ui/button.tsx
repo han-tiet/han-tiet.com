e5f90e6a130abdeadf9e396719010f174838f045
Han Tiet <hanweitiet@gmail.com>
2026-09-15 16:12:08 +0100

    feat: reduced button size on mobile and tablet

--- src/features/spotify-artist-collage/src/components/ui/button.tsx
+++ src/features/spotify-artist-collage/src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-zinc-950 focus-visible:ring-zinc-950/50 focus-visible:ring-[3px] aria-invalid:ring-red-500/20 dark:aria-invalid:ring-red-500/40 aria-invalid:border-red-500 dark:focus-visible:border-zinc-300 dark:focus-visible:ring-zinc-300/50 dark:aria-invalid:ring-red-900/20 dark:dark:aria-invalid:ring-red-900/40 dark:aria-invalid:border-red-900",
+  "inline-flex items-center justify-center gap-2 desktop:gap-3 whitespace-nowrap rounded-md desktop:rounded-[0.5rem] text-sm desktop:text-[1.25rem] font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-zinc-950 focus-visible:ring-zinc-950/50 focus-visible:ring-[3px] aria-invalid:ring-red-500/20 dark:aria-invalid:ring-red-500/40 aria-invalid:border-red-500 dark:focus-visible:border-zinc-300 dark:focus-visible:ring-zinc-300/50 dark:aria-invalid:ring-red-900/20 dark:dark:aria-invalid:ring-red-900/40 dark:aria-invalid:border-red-900",
   {
     variants: {
       variant: {
@@ -22,12 +22,13 @@ const buttonVariants = cva(
         link: "text-zinc-900 underline-offset-4 hover:underline dark:text-zinc-50",
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
-        icon: "size-9",
-        "icon-sm": "size-8",
-        "icon-lg": "size-10",
+        default:
+          "h-9 px-4 py-2 has-[>svg]:px-3 desktop:h-[3.25rem] desktop:px-6 desktop:py-3 desktop:has-[>svg]:px-[1.25rem]",
+        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5 desktop:h-12 desktop:gap-[0.5rem] desktop:px-[1.25rem] desktop:has-[>svg]:px-[1rem]",
+        lg: "h-10 rounded-md px-6 has-[>svg]:px-4 desktop:h-15 desktop:px-9 desktop:has-[>svg]:px-6",
+        icon: "size-9 desktop:size-[3.25rem]",
+        "icon-sm": "size-8 desktop:size-12",
+        "icon-lg": "size-10 desktop:size-15",
       },
     },
     defaultVariants: {
