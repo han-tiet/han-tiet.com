12721f73ecaa4950c7f1fe8b9e012c674c2df85f
Han Tiet <hanweitiet@gmail.com>
2026-09-16 14:08:09 +0100

    feat: made login page look consistent across devices

--- src/app/projects/spotify-artist-collage/login/page.tsx
+++ src/app/projects/spotify-artist-collage/login/page.tsx
@@ -5,13 +5,13 @@ import { ROUTES } from "@/constants/routes";
 
 export default function LoginPage() {
   return (
-    <div className="grid grid-col-12 min-h-[55vh] desktop:min-h-[45vh]">
-      <div className="flex justify-start items-center min-h-[5rem] px-4 text-sm md:text-lg desktop:text-xl hover:text-primary">
+    <div className="grid grid-col-12">
+      <div className="flex justify-start items-center min-h-[4rem] px-4 text-sm md:text-lg desktop:text-xl hover:text-primary">
         <a href={ROUTES.PROJECTS}>&lt;&lt; Projects</a>
       </div>
-      <div className="flex flex-col justify-center items-center gap-8 desktop:gap-12 px-6 desktop:px-9 py-12 desktop:pt-108 text-center">
+      <div className="flex flex-col justify-center items-center gap-8 px-6 pt-36 md:pt-68 [@media(max-height:500px)_and_(orientation:landscape)]:pt-16 desktop:gap-12 desktop:px-9 desktop:pt-108 text-center">
         <div className="flex flex-col justify-center items-center gap-4 desktop:gap-6">
-          <div className="text-3xl sm:text-5xl md:text-7xl desktop:text-[6.75rem] font-bold">
+          <div className="text-3xl md:text-6xl desktop:text-[6.75rem] font-bold">
             Spotify Artist Collage
           </div>
           <div className="text-base sm:text-lg desktop:text-[1.75rem]">
