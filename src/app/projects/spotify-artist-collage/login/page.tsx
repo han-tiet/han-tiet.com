88a788888f16003d9f451087d881b2770fddf166
Han Tiet <hanweitiet@gmail.com>
2026-09-17 12:09:22 +0100

    feat: changed from grid to flexbox

--- src/app/projects/spotify-artist-collage/login/page.tsx
+++ src/app/projects/spotify-artist-collage/login/page.tsx
@@ -5,11 +5,11 @@ import { ROUTES } from "@/constants/routes";
 
 export default function LoginPage() {
   return (
-    <div className="grid grid-col-12">
-      <div className="flex justify-start items-center min-h-[4rem] px-4 text-sm md:text-lg desktop:text-xl hover:text-primary">
+    <div className="flex flex-col desktop:min-h-dvh">
+      <div className="flex justify-start items-center min-h-16 px-4 text-sm md:text-lg desktop:text-xl hover:text-primary">
         <a href={ROUTES.PROJECTS}>&lt;&lt; Projects</a>
       </div>
-      <div className="flex flex-col justify-center items-center gap-8 px-6 pt-36 md:pt-68 [@media(max-height:500px)_and_(orientation:landscape)]:pt-16 desktop:gap-12 desktop:px-9 desktop:pt-108 text-center">
+      <div className="flex flex-col justify-center items-center text-center gap-8 px-6 pt-36 md:pt-68 [@media(max-height:500px)_and_(orientation:landscape)]:pt-16 desktop:gap-12 desktop:px-8 desktop:py-auto">
         <div className="flex flex-col justify-center items-center gap-4 desktop:gap-6">
           <div className="text-3xl md:text-6xl desktop:text-[6.75rem] font-bold">
             Spotify Artist Collage
